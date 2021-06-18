ddab98274495e9d0777b9aa934c5d18b22a0f995
israel tekahun <israelheaven@yahoo.com>
2021-06-18 13:04:05 +0200

    done

--- övn2/Program.cs
+++ övn2/Program.cs
@@ -5,12 +5,24 @@ namespace övn2
 {
     class Program
     {
+        private static bool isInteger(String input)
+        {
+            return int.TryParse(input, out int n);
+        }
 
+
+        private static void showMenu()
+        {
+            Console.WriteLine("\nEnter 0 to quit");
+            Console.WriteLine("Enter 1 to get ticket for cinema");
+            Console.WriteLine("Enter 2 to repeat a random text ten times");
+            Console.WriteLine("Enter 3 to get third word in a sentence");
+            Console.WriteLine("-----------");
+            Console.Write("choice: ");
+        }
         private static int calculateSinglePrice(int age)
         {
             int price = 0;
-
-            Console.WriteLine();
 
             if (age < 5 || age > 100)
             {
@@ -34,23 +46,11 @@ namespace övn2
 
             return price;
         }
-
-        private static void showMenu()
-        {
-            Console.WriteLine();
-            Console.WriteLine("Enter 0 to quit");
-            Console.WriteLine("Enter 1 get price");
-            Console.WriteLine("Eneter 2 family");
-            Console.WriteLine("-----------");
-            Console.Write("choice: ");
-        }
-        private static String getPrice()
+        private static void getPrice()
         {
             int age;
 
-            Console.WriteLine();
-
-            Console.Write("How many people? ");
+            Console.Write("\nHow many people? ");
             int people = int.Parse(Console.ReadLine());
             int counter = 0;
             int totalPrice = 0;
@@ -58,27 +58,63 @@ namespace övn2
             
             for (int i = 1; i <= people; i++)
             {
-                Console.WriteLine();
-                Console.Write($"Enter age for guest {i}: ");
-                age = int.Parse(Console.ReadLine());
+                Console.Write($"\nEnter age for guest {i}: ");
 
+                while(!int.TryParse(Console.ReadLine(), out age))
+                {
+                    Console.Write("Please enter a valide age: ");
+                }
+
+                Console.WriteLine("\n----------------");
                 totalPrice += calculateSinglePrice(age);
 
                 counter++;
 
-                Console.WriteLine();
-                Console.WriteLine("----------------");
-                Console.WriteLine($"{counter} guest(s): {totalPrice}kr");
-                Console.WriteLine("----------------");
-                Console.WriteLine();
+                Console.WriteLine($"{counter} guest(s), total price: {totalPrice}kr");
+                Console.WriteLine("----------------\n");
             }
-
-            return "  ";
         }
-        static void Main(string[] args)
+
+
+        private static void repeatText()
         {
             String input;
+            Console.Write("\nGive a random text: ");
+            input = Console.ReadLine();
+            Console.WriteLine();
+            for (int i = 1; i <= 10; i++)
+            {
+
+                Console.Write($"{i}. {input}");
+                if (i < 10)
+                    Console.Write(", ");
+            }
+            Console.WriteLine();
+        }
+
+        private static void splitSentence()
+        {
+
+            String input;
             String[] words;
+            Console.Write("\nGive a sentence with three or more words: ");
+            input = Console.ReadLine();
+
+            words = Regex.Split(input, "\\s+");
+
+            while (words.Length < 3)
+            {
+                Console.WriteLine("Give at least three words");
+                input = Console.ReadLine();
+                words = Regex.Split(input, "\\s+");
+            }
+
+
+            Console.WriteLine($"\nThe third word is: {words[2]}");
+        }
+
+        static void Main(string[] args)
+        {
 
             Console.WriteLine("Welcome to the main menu\nUse numbers to naviagte");
             bool done = false;
@@ -94,40 +130,20 @@ namespace övn2
                         getPrice();
                         break;
                     case "2":
-                        Console.WriteLine("Give a random text");
-                        input = Console.ReadLine();
-                        for (int i = 1; i <= 10; i++)
-                        {
-
-                            Console.Write($"{i}. {input}" );
-                            if(i < 10)
-                                Console.Write(", ");
-                        }
-
+                        repeatText();
                         break;
                     case "3":
-                        Console.WriteLine("Give a sentence with three words");
-                        input = Console.ReadLine();
-
-                        words = Regex.Split(input, "\\s+");
-
-                        while (words.Length < 3)
-                        {
-                            Console.WriteLine("Give at least three words");
-                            input = Console.ReadLine();
-                            words = Regex.Split(input, "\\s+");
-                        }
-
-                     
-                        Console.WriteLine($"The third word: {words[2]}");
-
+                        splitSentence();
                         break;
                     default:
-                        Console.WriteLine("wrong input");
+                        Console.WriteLine("\nWrong input: please enter 0 to 3");
                         break;
                 }
             } while (!done);
 
+            Console.WriteLine("\nGood bye!");
+
         }
+
     }
 }
