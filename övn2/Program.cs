509face5741cf7037a9cf526da344a2bf86d3985
israel tekahun <israelheaven@yahoo.com>
2021-06-18 12:11:42 +0200

    ..

--- övn2/Program.cs
+++ övn2/Program.cs
@@ -1,73 +1,132 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace övn2
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void showMenu()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Enter 0 to quit");
+            Console.WriteLine("Enter 1 get price");
+            Console.WriteLine("Eneter 2 family");
+            Console.WriteLine();
+        }
+            private static String getPrice()
         {
             int age;
+            Console.WriteLine("How many people?");
+            int people = int.Parse(Console.ReadLine());
+            int counter = 0;
+            int totalPrice = 0;
+
+            //gör det i en funktion
+            if (people == 1)
+            {
+
+                Console.WriteLine("Age ?");
+                age = int.Parse(Console.ReadLine());
+
+                if (age < 5 || age > 100)
+                {
+                    Console.WriteLine("Free enterance");
+                }
+                else if (age < 20)
+                {
+                    Console.WriteLine("Youth price: 80kr");
+                }
+                else if (age > 64)
+                {
+                    Console.WriteLine("Youth price: 90kr");
+                }
+                else
+                {
+                    Console.WriteLine("Youth price: 120kr");
+                }
+
+            }
+            else if (people > 1)
+            {
+                for (int i = 1; i <= people; i++)
+                {
+                    Console.WriteLine($"Enter age for guest {i}");
+                    age = int.Parse(Console.ReadLine());
+
+                    if (age < 20)
+                    {
+                        totalPrice += 80;
+                    }
+                    else if (age > 64)
+                    {
+                        totalPrice += 90;
+                    }
+                    else
+                    {
+                        totalPrice += 120;
+                    }
+                    counter++;
+                }
+                Console.WriteLine($"{counter} people: {totalPrice}kr");
+
+            }
+            else
+            {
+                // 0 or less given
+            }
+
+            return "  ";
+        }
+        static void Main(string[] args)
+        {
+            String input;
+            String[] words;
 
             Console.WriteLine("Welcome to the main menu\nUse numbers to naviagte");
-            Console.WriteLine("0 to quit");
-            Console.WriteLine("1 enter age");
-            Console.WriteLine("2 family");
-
             bool done = false;
             do
             {
+                showMenu();
                 switch (Console.ReadLine())
                 {
                     case "0":
                         done = true;
                         break;
                     case "1":
-                        Console.WriteLine("Age ?");
-                        age = int.Parse(Console.ReadLine());
 
-                        if (age < 5 || age > 100)
-                        {
-                            Console.WriteLine("Free enterance");
-                        }
-                        else if (age < 20)
-                        {
-                            Console.WriteLine("Youth price: 80kr");
-                        }
-                        else if (age > 64)
-                        {
-                            Console.WriteLine("Youth price: 90kr");
-                        }
-                        else
-                        {
-                            Console.WriteLine("Youth price: 120kr");
-                        }
+
+                        Console.WriteLine(getPrice());
+
+
                         break;
                     case "2":
-                        Console.WriteLine("How many people?");
-                        int people = int.Parse(Console.ReadLine());
-                        int counter = 0;
-                        int totalPrice = 0;
-
-                        for (int i = 1; i <= people; i++)
+                        Console.WriteLine("Give a random text");
+                        input = Console.ReadLine();
+                        for (int i = 1; i <= 10; i++)
                         {
-                            Console.WriteLine($"Enter age for guest {i}");
-                            age = int.Parse(Console.ReadLine());
 
-                            if (age < 20)
-                            {
-                                totalPrice += 80;
-                            }
-                            else if (age > 64)
-                            {
-                                totalPrice += 90;
-                            }
-                            else
-                            {
-                                totalPrice += 120;
-                            }
-                            counter++;
+                            Console.Write($"{i}. {input}" );
+                            if(i < 10)
+                                Console.Write(", ");
                         }
-                        Console.WriteLine($"{counter} people: {totalPrice}kr");
+
+                        break;
+                    case "3":
+                        Console.WriteLine("Give a sentence with three words");
+                        input = Console.ReadLine();
+
+                        words = Regex.Split(input, "\\s+");
+
+                        while (words.Length < 3)
+                        {
+                            Console.WriteLine("Give at least three words");
+                            input = Console.ReadLine();
+                            words = Regex.Split(input, "\\s+");
+                        }
+
+                     
+                        Console.WriteLine($"The third word: {words[2]}");
+
                         break;
                     default:
                         Console.WriteLine("wrong input");
